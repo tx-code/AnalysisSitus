7371f2eb0af6fdf8e0edd688b245a0e31f5877bc
Quaoar-machine <sergey.slyadnev@gmail.com>
2021-07-14 23:35:35 +0300

    Adjust jenkins.

--- jenkins-configure-and-build.bat
+++ jenkins-configure-and-build.bat
@@ -56,6 +56,7 @@ echo "*** Running CMake configuration..."
 
 "%JENKINS_CMAKE_BIN%\cmake.exe" -G"Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release ^
 -DUSE_CONSOLE=false -DUSE_THREADING=false -DUSE_RAPIDJSON=true -DUSE_FBX_SDK=true -DUSE_MOBIUS=true ^
+-D3RDPARTY_mobius_DIR=%JENKINS_3RDPARTIES_MOBIUS_DIR% ^
 -D3RDPARTY_DIR:PATH=%JENKINS_3RDPARTIES% -DINSTALL_DIR:PATH=../cmake-install-dir ../
 
 REM ===========================================================================
