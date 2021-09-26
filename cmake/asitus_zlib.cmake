b63024c0a6aba895d1ecca593941efd112b29405
Quaoar-machine <sergey.slyadnev@gmail.com>
2021-09-26 15:22:28 +0300

    Configure zlib as NetGen dep.

---
+++ cmake/asitus_zlib.cmake
@@ -0,0 +1,19 @@
+#--------------------------------------------------------------------------
+# zlib
+#--------------------------------------------------------------------------
+
+if (WIN32)
+  ASITUS_THIRDPARTY_PRODUCT("zlib" "" "zlib.h" "zlib" "")
+else()
+  ASITUS_THIRDPARTY_PRODUCT("zlib" "" "zlib.h" "libz" "")
+endif()
+
+message (STATUS "... zlib Include dirs: ${3RDPARTY_zlib_INCLUDE_DIR}")
+message (STATUS "... zlib Library dirs: ${3RDPARTY_zlib_LIBRARY_DIR}")
+message (STATUS "... zlib Binary  dirs: ${3RDPARTY_zlib_DLL_DIR}")
+
+set (3RDPARTY_zlib_LIBRARY_DIR_DEBUG "${3RDPARTY_zlib_LIBRARY_DIR}" CACHE INTERNAL "" FORCE)
+set (3RDPARTY_zlib_DLL_DIR_DEBUG "${3RDPARTY_zlib_DLL_DIR}" CACHE INTERNAL "" FORCE)
+
+message (STATUS "... zlib Debug Library dirs: ${3RDPARTY_zlib_LIBRARY_DIR_DEBUG}")
+message (STATUS "... zlib Debug Binary  dirs: ${3RDPARTY_zlib_DLL_DIR_DEBUG}")
