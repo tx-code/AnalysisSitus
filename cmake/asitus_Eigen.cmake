aed2608d466b58af887e10c7cf50d9432b7f127b
Quaoar <sergey.slyadnev@gmail.com>
2022-02-20 17:11:27 +0300

    Try to fix eigen checker.

--- cmake/asitus_Eigen.cmake
+++ cmake/asitus_Eigen.cmake
@@ -4,7 +4,7 @@ ASITUS_FIND_PRODUCT_DIR ("${3RDPARTY_DIR}" "eigen" EIGEN_DIR)
 
 set (3RDPARTY_EIGEN_DIR "${3RDPARTY_DIR}/${EIGEN_DIR}" CACHE PATH "The directory containing Eigen" FORCE)
 
-if (NOT EXISTS "${3RDPARTY_EIGEN_DIR}/Eigen/Eigen")
+if (NOT EXISTS "${3RDPARTY_EIGEN_DIR}/Eigen")
   set (3RDPARTY_EIGEN_DIR "" CACHE PATH "" FORCE)
   message( SEND_ERROR "... Eigen is not found.")
 endif()     
