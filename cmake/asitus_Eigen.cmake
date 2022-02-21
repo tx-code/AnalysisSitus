c79513ea700f1511455db43e638c794f82b1c2cf
Quaoar <sergey.slyadnev@gmail.com>
2022-02-21 08:43:10 +0300

    Try using find_package() for Eigen3.

--- cmake/asitus_Eigen.cmake
+++ cmake/asitus_Eigen.cmake
@@ -2,11 +2,10 @@ message (STATUS "Processing Eigen 3-rd party")
 
 ASITUS_FIND_PRODUCT_DIR ("${3RDPARTY_DIR}" "eigen" EIGEN_DIR)
 
-set (3RDPARTY_EIGEN_DIR "${3RDPARTY_DIR}/${EIGEN_DIR}" CACHE PATH "The directory containing Eigen" FORCE)
+find_package( Eigen3 REQUIRED )
+include_directories( EIGEN3_INCLUDE_DIR )
 
-if (NOT EXISTS "${3RDPARTY_EIGEN_DIR}/Eigen")
-  set (3RDPARTY_EIGEN_DIR "" CACHE PATH "" FORCE)
-  message( SEND_ERROR "... Eigen is not found.")
-endif()     
+set (3RDPARTY_EIGEN_DIR "${EIGEN3_INCLUDE_DIR}" CACHE PATH "The directory containing Eigen")
 
 message (STATUS "... Eigen dir: ${3RDPARTY_EIGEN_DIR}")
+message (STATUS "... Eigen dir: ${EIGEN3_INCLUDE_DIR}")
