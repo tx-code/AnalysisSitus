498915d644fb6a249cd56894aa56e17aa4ed9208
Quaoar <sergey.slyadnev@gmail.com>
2022-02-21 08:54:12 +0300

    Try fixing cmake.

--- cmake/asitus_Eigen.cmake
+++ cmake/asitus_Eigen.cmake
@@ -1,11 +1,19 @@
 message (STATUS "Processing Eigen 3-rd party")
 
-ASITUS_FIND_PRODUCT_DIR ("${3RDPARTY_DIR}" "eigen" EIGEN_DIR)
+if (WIN32)
+  ASITUS_FIND_PRODUCT_DIR ("${3RDPARTY_DIR}" "eigen" EIGEN_DIR)
+else()
+  find_package (Eigen3 REQUIRED)
+  include_directories (EIGEN3_INCLUDE_DIR)
 
-find_package( Eigen3 REQUIRED )
-include_directories( EIGEN3_INCLUDE_DIR )
+  set (3RDPARTY_EIGEN_DIR "${EIGEN3_INCLUDE_DIR}" CACHE INTERNAL "" FORCE)
+endif()
 
 set (3RDPARTY_EIGEN_DIR "${EIGEN3_INCLUDE_DIR}" CACHE PATH "The directory containing Eigen")
 
+if (NOT EXISTS "${3RDPARTY_EIGEN_DIR}/Eigen/Eigen")
+  set (3RDPARTY_EIGEN_DIR "" CACHE PATH "" FORCE)
+  message( SEND_ERROR "... Eigen is not found.")
+endif() 
+
 message (STATUS "... Eigen dir: ${3RDPARTY_EIGEN_DIR}")
-message (STATUS "... Eigen dir: ${EIGEN3_INCLUDE_DIR}")
