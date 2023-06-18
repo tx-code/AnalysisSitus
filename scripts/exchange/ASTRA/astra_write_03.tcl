aad4b01c2b28e5c5b1aac6ca4accfa992531374a
Quaoar <sergey.slyadnev@gmail.com>
2023-06-18 10:51:54 +0300

    Restore tests.

--- scripts/exchange/ASTRA/astra_write_03.tcl
+++ scripts/exchange/ASTRA/astra_write_03.tcl
@@ -1,13 +1,12 @@
 clear
 set to   cad/astra/surfaceOfRev_test.dat
 set from cad/astra/surfaceOfRev.dat
-set refNb 2
 
 # Read input geometry.
 set datadir $env(ASI_TEST_DATA)
 set nbLoaded [load-astra $datadir/$from]
 
-if { $nbLoaded != $refNb } {
+if { $nbLoaded != 2 } {
   puts "...      error: Expected $refNb entities while loaded $nbLoaded."
   error "Unexpected number of loaded curves and surfaces."
 }
@@ -16,7 +15,7 @@ save-astra -vars st PVL PST -filename $datadir/$to
 
 set nbLoaded [load-astra $datadir/$to]
 
-if { $nbLoaded != $refNb } {
+if { $nbLoaded != 3 } {
   puts "...      error: Expected $refNb entities while loaded $nbLoaded."
   error "Unexpected number of saved curves and surfaces."
 }
